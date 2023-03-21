4e6a878b508b2e315dd28bcdcb93cd789a01a75d
Thomas Song <thomas.song@efrei.net>
2023-03-21 10:43:04 +0100

    initial extension file

--- openaiwhisperTest.py
+++ openaiwhisperTest.py
@@ -2,10 +2,32 @@ import whisper
 import torch
 import os
 from youtube import Youtube
+import threading
 
-devices = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+def transcribe(model, options, file_path):
+    result = model.transcribe(file_path, **options)
+    return result
+
+def print_text(yt, result):
+    text = yt.whisper_result_to_text(result)
+    print(text)
+
+devices = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = whisper.load_model("small", device=devices)
 yt = Youtube("https://www.youtube.com/watch?v=CqeACWm74xI")
 options = {"fp16": False, "task": "translate"}
-result = model.transcribe(os.path.join('data','audio.wav'), **options)
-print(yt.whisper_result_to_text(result))
+
+# Start a separate thread for transcribing
+transcribe_thread = threading.Thread(target=transcribe, args=(model, options, os.path.join('data','audio.wav')))
+transcribe_thread.start()
+
+# Wait for the transcription thread to finish and get the result
+transcribe_thread.join()
+result = transcribe_thread
+
+# Start a separate thread for printing
+print_thread = threading.Thread(target=print_text, args=(yt, result))
+print_thread.start()
+
+# Wait for the printing thread to finish before exiting
+print_thread.join()
