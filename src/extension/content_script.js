928be93468366f3c77b4c4df7f01e17e2a07f13c
Thomas Song <thomas.song@efrei.net>
2023-04-26 18:26:55 +0200

    usage of polling for sending transcription

--- src/extension/content_script.js
+++ src/extension/content_script.js
@@ -2,36 +2,60 @@
     
     let youtubeLeftControls, youtubePlayer;
     let currentVideo = null;
+    let videoTranscripted = false;
 
     chrome.runtime.onMessage.addListener((msg) => {
         if (msg.type === 'NEW_URL') {
             currentVideo = msg.videoId;
-            loadNewVideo();
+            let videoTranscripted = false;
+            setInterval(checkTranscription, 5000);
+            transcribing()
+            
         }
     });
 
-    const loadNewVideo = () => {
-        // Connect to the backend server using a named port
-        
+    const transcribing = () => {
         fetch('http://127.0.0.1:5000/transcribe', {
             method: 'POST',
             mode: 'cors',
             headers: {
                 'Content-Type': 'application/json'
-            },
+                },
             body: JSON.stringify({
                 video_id: currentVideo
             })
         })
         .then(response => response.text())
         .then(data => {
+            videoTranscripted = true;
             console.log(data);
         })
         .catch(error => {
             console.error(error);
         });
-        
-
     }
+
+    const checkTranscription = () => {
+        if (!videoTranscripted) {
+            fetch('http://127.0.0.1:5000/check_transcription',  {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+        else {
+            clearInterval(checkTranscription);
+        }
+    }
+
 
 })();
